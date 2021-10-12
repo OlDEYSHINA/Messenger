890ab3495d19035289ae387b768150e7b8f20560
PodkorytovSA <PodkorytovSA@elcomplus.ru>
2021-10-12 12:30:09 +0700

    Simple  UI client and server

---
+++ Messenger/Client/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebSocketSharp;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WebSocket client = new WebSocket("ws://127.0.0.1:60111/test");
+        public MainWindow()
+        {
+
+            InitializeComponent();
+           
+            client.OnMessage += Client_OnMessage;
+            client.Connect();
+
+            Console.WriteLine("Enter message:");
+
+           /* while (true)
+            {
+                client.Send(Console.ReadLine());
+            }*/
+
+        }
+        private static void Client_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine(e.Data);
+            Console.WriteLine("Enter message:");
+        }
+
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            client.Send(MessageTextBox.Text);
+        }
+    }
+}
---
+++ Messenger/Server/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socketServer = new WebSocketServer("ws://127.0.0.1:60111");
+
+            socketServer.AddWebSocketService<MainMessageHandler>("/test");
+
+            socketServer.Start();
+
+
+            if (socketServer.IsListening)
+            {
+                Console.WriteLine($"Listening on port {socketServer.Port}, and providing WebSocket services:");
+
+                foreach (var path in socketServer.WebSocketServices.Paths) Console.WriteLine($"- {path}");
+            }
+
+            Console.WriteLine("Servers started. Press the key to exit");
+            Console.ReadKey(true);
+        }
+        internal class MainMessageHandler : WebSocketBehavior
+        {
+            protected override void OnClose(CloseEventArgs e)
+            {
+                base.OnClose(e);
+
+                Console.WriteLine($"{e}The connection was closed.");
+            }
+
+            protected override void OnError(ErrorEventArgs e)
+            {
+                base.OnError(e);
+
+                Console.WriteLine($"Server error. Message: {e.Message}");
+            }
+
+            protected override void OnOpen()
+            {
+                base.OnOpen();
+
+                Console.WriteLine("The connection to main server was open.");
+            }
+
+            protected override void OnMessage(MessageEventArgs args)
+            {
+                if (string.IsNullOrEmpty(args.Data))
+                    return;
+                Console.WriteLine($"Message: {args.Data}");
+
+                Send($"{args.Data} : Server Ok");
+            }
+        }
+    }
+}
